0f62ce37d89ec59499a21bc6bb03e4104d680a68
Samuel Mozarth <65967088+samuelmozarthss@users.noreply.github.com>
2021-09-23 12:32:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Motivation - app em kotlin
---
+++ app/src/main/java/com/example/motivation/ui/SplashActivity.kt
@@ -0,0 +1,45 @@
+package com.example.motivation.ui
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.motivation.R
+import com.example.motivation.infra.SecuriryPreferences
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var mSecuriryPreferences: SecuriryPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar!!.hide()
+
+        mSecuriryPreferences = SecuriryPreferences(this)
+
+        buttonSave.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        val id = v?.id
+        if (id == R.id.buttonSave) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = editName.text.toString()
+
+        if (name != "") {
+            mSecuriryPreferences.storeString("name", name)
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, "Informe seu nome", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
